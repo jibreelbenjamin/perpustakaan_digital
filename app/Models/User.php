90d6cbfea69611982d7ee55b5a2b6e78b2e7c65b
jibreelbenjamin <sch.jibreelbenjamin.com>
2025-10-20 09:36:04 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+    protected $primaryKey = 'id_user';
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isModerator()
+    {
+        return $this->role === 'moderator';
+    }
+
+    public function isStaff()
+    {
+        return $this->role === 'staff';
+    }
+
+    public function pinjaman()
+    {
+        return $this->hasMany(Pinjaman::class, 'id_user', 'id_user');
+    }
+}
